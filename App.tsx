e12f4d1e9730cfa338045d0f4065690d9b50b37b
Roberto Fortes <robertbernd23@gmail.com>
2022-01-10 03:21:31 -0300

    feat: configuring react navigation and using tab navigation

--- App.tsx
+++ App.tsx
@@ -12,8 +12,7 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -29,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <AppRoutes />
     </ThemeProvider>
   );
 }
