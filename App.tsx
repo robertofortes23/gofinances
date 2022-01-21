dd07215874db5ed3536c0277797b52ed158cb815
Roberto Fortes <robertbernd23@gmail.com>
2022-01-20 22:01:05 -0300

    feat: creating hook with context

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ import theme from './src/global/styles/theme';
 
 import { AppRoutes } from './src/routes/app.routes';
 import { StatusBar } from 'react-native';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -37,7 +38,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
